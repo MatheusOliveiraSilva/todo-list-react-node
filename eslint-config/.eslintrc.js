4d2467df89bad9ab2e7a8f3b8ef3c1d8b5266326
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-07-26 22:44:32 -0300

    eslint config updated and standard config removed from eslint

--- eslint-config/.eslintrc.js
+++ eslint-config/.eslintrc.js
@@ -1,28 +1,30 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2020: true,
-    node: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard',
-    'prettier',
-    'prettier/standard',
-    'prettier/react',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 11,
-  },
-  plugins: ['react', 'prettier'],
-  rules: {},
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
+	env: {
+		browser: true,
+		commonjs: true,
+		es2020: true,
+		node: true,
+	},
+	extends: [
+		'plugin:prettier/recommended',
+		'plugin:react/recommended',
+		'prettier/react',
+		'prettier',
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 11,
+		sourceType: 'module',
+	},
+	plugins: ['react', 'prettier'],
+	rules: {
+		'prettier/prettier': 'error',
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
 }
