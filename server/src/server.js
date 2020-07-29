af1ca53cb0339701de2861bfb27c15256dcda982
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-07-28 22:58:23 -0300

    server connected to database and routes defined

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,34 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+
+const todo = require('./routes/todo')
+const user = require('./routes/user')
 
 const app = express()
 
+if (!config.get('jwtPrivateKey')) {
+	console.error('FATAL ERROR: jwtPrivateKey is not defined.')
+	process.exit(1)
+}
+
 app.use(express.json())
+
+app.use('/todo', todo)
+app.use('/user', user)
 
 app.get('/', (req, res) => {
 	res.send('Hello man')
 })
+
+const options = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: true,
+}
+
+mongoose.connect('mongodb://localhost/todo-list-react-node', options)
 
 const port = process.env.PORT || 9000
 
