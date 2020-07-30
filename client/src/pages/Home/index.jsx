a864637a5c7f7b5496bab10a88489e1ec353ed23
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-07-30 11:39:31 -0300

    main and its styles added in home and import arrow right with react icons feather

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FiArrowRight } from 'react-icons/fi'
 
 import { StyledHome } from './styles'
 
 function Home() {
+	const sourceProject =
+		'https://github.com/MatheusOliveiraSilva/todo-list-react-node'
+
 	return (
 		<StyledHome>
 			<header>
@@ -20,6 +24,17 @@ function Home() {
 
 				<button>Create Account</button>
 			</header>
+
+			<main>
+				<h1>Todo list App</h1>
+				<p>An app that creates list of todos to help yours daily needs.</p>
+				<button>
+					<strong>Know more about the project</strong>
+					<a href={sourceProject} target='_blank' rel='noreferrer'>
+						<FiArrowRight size='1.9em' />
+					</a>
+				</button>
+			</main>
 		</StyledHome>
 	)
 }
--- client/src/pages/Home/styles.js
+++ client/src/pages/Home/styles.js
@@ -2,7 +2,11 @@ import styled from 'styled-components'
 
 export const StyledHome = styled.div`
 	padding: 0 35px;
+	width: 100%;
 	height: 100vh;
+	box-sizing: border-box;
+	display: flex;
+	flex-direction: column;
 
 	header {
 		display: flex;
@@ -11,37 +15,98 @@ export const StyledHome = styled.div`
 
 		padding-top: 24px;
 		box-sizing: border-box;
+
+		button {
+			cursor: pointer;
+			border-radius: 4px;
+			border: none;
+			font-weight: 500;
+			background-color: #1c5d99;
+			color: #fff;
+			box-sizing: border-box;
+			width: 115.5px;
+			height: 35px;
+
+			&:hover {
+				background-color: transparent;
+				border: 1px solid #1c5d99;
+				color: #1c5d99;
+			}
+		}
 	}
 
 	ul {
 		display: flex;
 
 		li {
-			padding-right: 28px;
+			padding-right: 30px;
 
 			a {
 				cursor: pointer;
-				color: #304057;
+				color: #222222;
 			}
 		}
 	}
 
-	button {
-		cursor: pointer;
-		border-radius: 4px;
-		border: none;
-		font-weight: 500;
-		background-color: #304057;
-		color: #fff;
-		box-sizing: border-box;
-		width: 115.5px;
-		height: 35px;
+	main {
+		flex: 1;
+		width: 50%;
+		color: #222222;
+		padding-top: 2em;
 
-		&:hover {
-			background-color: transparent;
-			border: 1px solid #304057;
-			color: #304057;
+		h1 {
+			font-size: 3.2rem;
+			font-weight: 500;
+		}
+
+		p {
+			font-size: 1.7rem;
+			margin-bottom: 2.5em;
+		}
+
+		button {
+			width: 65%;
+			height: 55px;
+			display: flex;
+			align-items: center;
+			background-color: #5d8cce;
+			border: none;
+			color: #fff;
+			border-radius: 8px 25px;
+
+			transition: 3s;
+
+			&:hover {
+				background-color: #266eb1;
+
+				a {
+					background-color: #2364a0;
+				}
+			}
+
+			strong {
+				font-family: Arial;
+				flex: 1;
+				font-size: 1rem;
+			}
+
+			a {
+				width: 55px;
+				height: 100%;
+				background-color: #5086d3;
+				border-bottom-right-radius: 8px;
+				border-top-right-radius: 25px;
+
+				display: flex;
+				align-items: center;
+				justify-content: center;
+
+				transition: 3s;
+				cursor: pointer;
+				color: #fff;
+			}
 		}
 	}
 `
 // color --> #7C7C7C
+// color -->
