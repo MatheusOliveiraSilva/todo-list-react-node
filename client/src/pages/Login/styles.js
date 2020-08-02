2a9fbc9b6f4ccb0ca0f3eaa41f78c92729be4fa2
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-08-02 20:56:55 -0300

    Login's styles

--- client/src/pages/Login/styles.js
+++ client/src/pages/Login/styles.js
@@ -24,12 +24,13 @@ export const LoginButton = styled.button`
 	width: 100%;
 	font-weight: 500;
 	border: none;
+	border-radius: 3px;
 `
 
 export const StyledLogin = styled.div`
 	width: 100%;
 	height: 100vh;
-	background-color: #fbfbfb;
+	background-color: #f3f3f3;
 	display: flex;
 	align-items: center;
 	justify-content: center;
@@ -37,7 +38,7 @@ export const StyledLogin = styled.div`
 	form {
 		width: 400px;
 		height: 75%;
-		box-shadow: 2px 2px 15px #ccc;
+		box-shadow: 2px 2px 15px rgba(0, 0, 0, 0.25);
 		background-color: #fff;
 		border-radius: 7px;
 		padding: 1em 1.6em;
@@ -70,16 +71,17 @@ export const StyledLogin = styled.div`
 			}
 
 			input {
-				height: 40px;
+				height: 43px;
 				/* border: 1px solid #838383; */
 				border: none;
 				outline: none;
-				background-color: #fbfbfb;
-				box-shadow: inset 0 0 3px #ccc;
+				background-color: #f3f3f3;
+				box-shadow: inset 0 2px 3px rgba(0, 0, 0, 0.2);
 				box-sizing: border-box;
 				border-width: 0;
 				transition: border-width 0.4s;
 				padding: 0 6px;
+				border-radius: 3px;
 
 				&:focus {
 					border-width: 3px;
