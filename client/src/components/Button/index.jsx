3ed1c0fc9b561b05f2940aef2e9ee411f02f4ec3
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-08-10 23:42:55 -0300

    Button component created

---
+++ client/src/components/Button/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { StyledButton } from './styles'
+
+function Button({ children }) {
+	return <StyledButton>{children}</StyledButton>
+}
+
+Button.propTypes = {
+	children: PropTypes.node,
+}
+
+export default Button
