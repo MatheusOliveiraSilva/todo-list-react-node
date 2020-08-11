b92f4374bd9e14ab61db8ed6128e56aa5bf93ed3
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-08-10 23:43:28 -0300

    Button X component created

---
+++ client/src/components/ButtonX/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { FiX } from 'react-icons/fi'
+import PropTypes from 'prop-types'
+
+import { StyledButtonX } from './styles'
+
+function ButtonX({ route }) {
+	return (
+		<StyledButtonX to={route}>
+			<FiX size='1.5em' />
+		</StyledButtonX>
+	)
+}
+
+ButtonX.propTypes = {
+	route: PropTypes.string.isRequired,
+}
+
+export default ButtonX
---
+++ client/src/components/ButtonX/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+export const StyledButtonX = styled(Link)`
+	position: absolute;
+	right: 15px;
+	cursor: pointer;
+`
