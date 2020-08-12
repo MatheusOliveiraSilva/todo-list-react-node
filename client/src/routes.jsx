ee3e387c8fead75f75542431bc61e825266aae0c
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-08-12 18:05:32 -0300

    export pages and add to the Route

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -4,6 +4,8 @@ import { Route, BrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import CreateAccount from './pages/CreateAccount'
+import Hello from './pages/Hello'
+import Todo from './pages/Todo'
 
 function Routes() {
 	return (
@@ -11,6 +13,8 @@ function Routes() {
 			<Route component={Home} path='/' exact />
 			<Route component={Login} path='/login' />
 			<Route component={CreateAccount} path='/create-account' />
+			<Route component={Hello} path='/hello' />
+			<Route component={Todo} path='/todos' />
 		</BrowserRouter>
 	)
 }
