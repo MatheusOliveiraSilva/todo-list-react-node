d86af2b53de2faf7fb4dbe2355f7a8af7ee1c74c
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-08-14 21:34:25 -0300

    import api and useState, handleSubmit

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,24 +1,61 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import api from './../../services/api'
 
 import Input from '../../components/Input'
 import Redirect from '../../components/Redirect'
 import Button from '../../components/Button'
 import ButtonX from '../../components/ButtonX'
 
-import { StyledLogin, LoginButton, XButton } from './styles'
+import { StyledLogin } from './styles'
 
 function Login() {
+	// ursobear
+	// --> authorization routes react
+	const history = useHistory()
+
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+
+	function handleLogin(e) {
+		e.preventDefault()
+
+		api
+			.post('user/authenticate', {
+				username,
+				password,
+			})
+			.then(() => {
+				history.push('/todos')
+			})
+			.catch(() => {
+				alert('Something was wrong!')
+			})
+	}
+
 	return (
 		<StyledLogin>
-			<form>
+			<form onSubmit={handleLogin}>
 				<ButtonX route='/' />
 
 				<fieldset>
 					<legend>Login to Continue</legend>
 
-					<Input label='Username' name='username' autoComplete='no' autoFocus />
-					<Input label='Password' name='password' type='password' />
+					<Input
+						label='Username'
+						name='username'
+						autoComplete='no'
+						autoFocus
+						value={username}
+						onChange={e => setUsername(e.target.value)}
+					/>
+					<Input
+						label='Password'
+						name='password'
+						type='password'
+						value={password}
+						onChange={e => setPassword(e.target.value)}
+					/>
 				</fieldset>
 
 				<Redirect route='/' label='Forgot Password?' />
