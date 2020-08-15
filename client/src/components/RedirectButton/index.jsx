5febf2aa4aece8b5fdd2112d1e2160c4778af587
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-08-14 22:23:02 -0300

    RedirectButton component created

---
+++ client/src/components/RedirectButton/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { StyledLink } from './styles'
+
+function RedirectButton({ route, label }) {
+	return <StyledLink to={route}>{label}</StyledLink>
+}
+
+RedirectButton.propTypes = {
+	route: PropTypes.string.isRequired,
+	label: PropTypes.string.isRequired,
+}
+
+export default RedirectButton
