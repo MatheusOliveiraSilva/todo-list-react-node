70c047b32d48dc9d454244a9652709f236b9f885
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-08-14 21:36:06 -0300

    import api, useState and TodoList component

--- client/src/pages/Todo/index.jsx
+++ client/src/pages/Todo/index.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import api from './../../services/api'
 
 import TodoItem from '../../components/TodoItem'
+import TodoList from '../../components/TodoList'
 
 import { StyledTodo } from './styles'
 
 function Todo() {
+	const [lists, setLists] = useState([])
+
 	return (
 		<StyledTodo>
 			<header>Hello User</header>
 
 			<main>
-				<TodoItem label='Wash the dishes' />
+				<TodoList>
+					<TodoItem label='Wash the dishes' />
+				</TodoList>
 			</main>
 		</StyledTodo>
 	)
