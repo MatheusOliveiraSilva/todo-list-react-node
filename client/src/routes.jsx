cc6f074a25c9f6666955b2115532fb1b317896e3
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-08-16 21:58:27 -0300

    PrivateRoute added

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,5 +1,9 @@
+/* eslint-disable prettier/prettier */
 import React from 'react'
-import { Route, BrowserRouter } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import { Route, BrowserRouter, Redirect, Switch } from 'react-router-dom'
+
+import { isAuthenticated } from './services/auth'
 
 import Home from './pages/Home'
 import Login from './pages/Login'
@@ -7,16 +11,36 @@ import CreateAccount from './pages/CreateAccount'
 import Hello from './pages/Hello'
 import Todo from './pages/Todo'
 
+const PrivateRoute = ({ component: Component, ...rest }) => (
+	<Route
+		{...rest}
+		render={props =>
+			isAuthenticated() ? (
+				<Component {...props} />
+			) : (
+					<Redirect to={{ pathname: '/', state: { from: props.location } }} />
+				)
+		}
+	/>
+)
+
 function Routes() {
 	return (
 		<BrowserRouter>
-			<Route component={Home} path='/' exact />
-			<Route component={Login} path='/login' />
-			<Route component={CreateAccount} path='/create-account' />
-			<Route component={Hello} path='/hello' />
-			<Route component={Todo} path='/todos' />
+			<Switch>
+				<Route component={Home} path='/' exact />
+				<Route component={Login} path='/login' />
+				<Route component={CreateAccount} path='/create-account' />
+				<Route component={Hello} path='/hello' />
+				<PrivateRoute component={Todo} path='/app' />
+			</Switch>
 		</BrowserRouter>
 	)
+}
+
+PrivateRoute.propTypes = {
+	component: PropTypes.element.isRequired,
+	location: PropTypes.object.isRequired,
 }
 
 export default Routes
