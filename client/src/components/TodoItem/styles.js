40bb583f5f9c03cd5d94e70d1ea1572c05682e13
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-08-18 21:16:10 -0300

    TodoItem box-shadow transiton and hover and Text styling based on the props

--- client/src/components/TodoItem/styles.js
+++ client/src/components/TodoItem/styles.js
@@ -1,14 +1,19 @@
 import styled from 'styled-components'
 
 export const StyledTodoItem = styled.div`
-	box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.3);
+	box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.3);
 	border-radius: 1.7em;
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	padding: 0 1.5em;
+	padding: 0 1em 0 1.5em;
 	height: 50px;
 	box-sizing: border-box;
+	transition: box-shadow 0.3s;
+
+	&:hover {
+		box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.3);
+	}
 `
 
 export const Text = styled.div`
@@ -16,4 +21,7 @@ export const Text = styled.div`
 	font-size: 1.2rem;
 	flex: 1;
 	font-weight: 300;
+
+	text-decoration: ${props => (props.status ? 'line-through' : 'none')};
+	color: ${props => (props.status ? '#646e78' : 'inherit')};
 `
