a65c9817e80d73dd15452e69d0bc740c498eba58
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-08-18 21:22:37 -0300

    completing the page

--- client/src/pages/Todo/index.jsx
+++ client/src/pages/Todo/index.jsx
@@ -1,26 +1,69 @@
+/* eslint-disable prettier/prettier */
 import React, { useState, useEffect } from 'react'
+import { FiPlus } from 'react-icons/fi'
 import api from './../../services/api'
+import { Link } from 'react-router-dom'
 
 import TodoItem from '../../components/TodoItem'
 import TodoList from '../../components/TodoList'
 
-import { StyledTodo } from './styles'
+import { StyledTodo, Message, BackButton } from './styles'
+
+import { getUser } from './../../services/auth'
+import Button from '../../components/Button'
 
 function Todo() {
 	const [lists, setLists] = useState([])
+	const [user, setUser] = useState({})
 
 	useEffect(() => {
-		api.get('/todo').then(response => console.log(response))
+		api.get('/todo').then(response => {
+			setLists(response.data)
+			console.log(response.data)
+			setUser(JSON.parse(getUser()))
+		})
 	}, [])
+
+	async function handleNewList() {
+		// dropdown with input in
+
+		try {
+			await api.post('/todo', { name })
+		} catch (err) {
+			alert(err)
+		}
+
+	}
 
 	return (
 		<StyledTodo>
-			<header>Hello User</header>
+			<header>
+				<Link to='/'>
+					<BackButton strokeWidth='1.5px' />
+				</Link>
+
+				<div>
+					Hello {user.username ? user.username : 'User'}
+				</div>
+
+				<div>
+					<Button>
+						<FiPlus size='1.3em' strokeWidth='2.5px' />
+						New list
+					</Button>
+				</div>
+			</header>
 
 			<main>
-				<TodoList>
-					<TodoItem label='Wash the dishes' />
-				</TodoList>
+				{lists.map(list => (
+					<TodoList key={list._id} name={list.name}>
+						{list.tasks.length === 0 ? (
+							<Message>Empty list</Message>
+						) : (
+								list.tasks.map(task => <TodoItem key={task} label={task} />)
+							)}
+					</TodoList>
+				))}
 			</main>
 		</StyledTodo>
 	)
--- client/src/pages/Todo/styles.js
+++ client/src/pages/Todo/styles.js
@@ -1,18 +1,76 @@
 import styled from 'styled-components'
 
+import { FiArrowLeft } from 'react-icons/fi'
+
+export const BackButton = styled(FiArrowLeft)`
+	cursor: pointer;
+	color: #4f5165;
+	transition: color 0.3s;
+
+	&:hover {
+		/* color: #646e78; */
+		color: #aea3b0;
+	}
+`
+
+export const Message = styled.div`
+	color: #646e78;
+	font-style: italic;
+`
+
 export const StyledTodo = styled.div`
 	width: 100vw;
 	height: 100vh;
 
 	header {
 		font-size: 2rem;
-		padding: 0.8em 0;
+		padding: 0.8em 1em;
 		text-align: center;
 		font-weight: 300;
+		display: grid;
+		grid-template-rows: auto;
+		grid-template-columns: 1fr 3fr 1fr;
+
+		a {
+			display: flex;
+			justify-content: flex-start;
+			align-items: center;
+		}
+
+		div:nth-child(2) {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+
+		div:last-child {
+			display: flex;
+			justify-content: flex-end;
+			align-items: center;
+
+			button {
+				width: 125px;
+				background-color: #1f92ee;
+				color: #fff;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+
+				&:hover {
+					background-color: #1c5d99;
+				}
+
+				svg {
+					margin-right: 5px;
+				}
+			}
+		}
 	}
 
 	main {
 		width: 55%;
 		margin: 1em auto;
+
+		color: #293132;
 	}
 `
