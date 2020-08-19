4670a253ff4e8650bfdfa552b1b8361bd689e5f9
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-08-18 21:24:16 -0300

    localStorage configuration defined

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -1,5 +1,12 @@
 export const TOKEN_KEY = '@todolist_token'
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null
 export const getToken = () => localStorage.getItem(TOKEN_KEY)
-export const login = token => localStorage.setItem(TOKEN_KEY, token)
-export const logout = () => localStorage.removeItem(TOKEN_KEY)
+export const login = (token, user) => {
+	localStorage.setItem(TOKEN_KEY, token)
+	localStorage.setItem('user', JSON.stringify(user))
+}
+export const getUser = () => localStorage.getItem('user')
+export const logout = () => {
+	localStorage.removeItem(TOKEN_KEY)
+	localStorage.removeItem('user')
+}
