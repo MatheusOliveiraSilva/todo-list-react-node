fa5f70fcadd527f09be0293d8106e0d24d5cf130
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-08-21 23:22:23 -0300

    TodoList functionalities introduced

--- client/src/components/TodoList/index.jsx
+++ client/src/components/TodoList/index.jsx
@@ -1,13 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import { FiPlus, FiEdit, FiTrash2 } from 'react-icons/fi'
+import api from './../../services/api'
 
-import { StyledTodoList } from './styles'
+import {
+	StyledTodoList,
+	TodoListHeader,
+	ButtonAction,
+	ButtonGroup,
+	TodoListForm,
+	Content,
+} from './styles'
 
-function TodoList({ children, name }) {
+function TodoList({ children, name, id }) {
+	const [inputStatus, setInputStatus] = useState(false)
+
+	function handleInputString() {
+		setInputStatus(!inputStatus)
+	}
+
+	async function handleAddTask() {
+		api.put(`/todo/rename/${id}`, { task: something })
+		// how get the ID
+	}
+
 	return (
 		<StyledTodoList>
-			<h2>{name}</h2>
-			<div>{children}</div>
+			<TodoListHeader>
+				<h2>{name}</h2>
+				<ButtonGroup>
+					<ButtonAction onClick={handleInputString}>
+						<FiPlus size='1.5em' />
+					</ButtonAction>
+
+					<ButtonAction onClick={handleInputString}>
+						<FiEdit size='1.5em' />
+					</ButtonAction>
+
+					<ButtonAction>
+						<FiTrash2 size='1.5em' />
+					</ButtonAction>
+				</ButtonGroup>
+			</TodoListHeader>
+			<TodoListForm showInput={inputStatus ? 1 : 0}>
+				<input type='text' />
+				<button type='submit'></button>
+				{/* TWO INPUTS */}
+			</TodoListForm>
+			<Content>{children}</Content>
 		</StyledTodoList>
 	)
 }
@@ -15,6 +55,7 @@ function TodoList({ children, name }) {
 TodoList.propTypes = {
 	children: PropTypes.node,
 	name: PropTypes.string.isRequired,
+	id: PropTypes.string,
 }
 
 export default TodoList
--- client/src/components/TodoList/styles.js
+++ client/src/components/TodoList/styles.js
@@ -1,9 +1,74 @@
 import styled from 'styled-components'
 
+export const TodoListHeader = styled.div`
+	display: flex;
+	align-items: center;
+`
+
+export const ButtonAction = styled.button`
+	background: none;
+	border: none;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	cursor: pointer;
+	outline: 0;
+
+	svg {
+		stroke-width: 2px;
+		color: #4f5165;
+
+		&:hover {
+			color: #aea3b0;
+		}
+	}
+`
+
+export const ButtonGroup = styled.div`
+	display: none;
+
+	button {
+		margin-right: 15px;
+
+		&:last-child {
+			margin-right: 0;
+		}
+	}
+`
+
+export const Content = styled.div`
+	margin-top: 0.8em;
+`
+
 export const StyledTodoList = styled.div`
 	h2 {
 		font-weight: 300;
+		margin-right: 0.9em;
+		margin-top: 0;
+		margin-bottom: 0;
 	}
 
 	padding-bottom: 3em;
+
+	&:hover {
+		${ButtonGroup} {
+			display: flex;
+		}
+	}
+`
+
+export const TodoListForm = styled.form`
+	height: ${props => (props.showInput ? '45px' : '0')};
+	display: flex;
+	align-items: center;
+	overflow: hidden;
+	transition: height 0.3s;
+
+	input {
+		outline: 0;
+		width: 200px;
+		height: 19px;
+		font-size: 0.9rem;
+		font-family: 'Ubuntu', sans-serif;
+	}
 `
