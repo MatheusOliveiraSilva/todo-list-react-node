c39de4a66359ca13ba178c0c7d71aafcab97c1e1
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-08-26 20:25:12 -0300

    dependencies property and check box consequencies added

--- client/src/components/TodoItem/index.jsx
+++ client/src/components/TodoItem/index.jsx
@@ -4,11 +4,20 @@ import PropTypes from 'prop-types'
 import { StyledTodoItem, Text } from './styles'
 import Checkbox from '../Checkbox'
 
-function TodoItem({ label, ...rest }) {
+function TodoItem({ label, dependencies, ...rest }) {
 	const [checked, setChecked] = useState(false)
 
+	const { setCheckedTasks, checkedTasks, setInnerViewOptionsBar } = dependencies
+
 	function handleCheckBoxChange(event) {
+		if (!event.target.checked) setCheckedTasks(checkedTasks - 1)
+
 		setChecked(event.target.checked)
+
+		if (event.target.checked) {
+			setCheckedTasks(checkedTasks + 1)
+			setInnerViewOptionsBar(true)
+		}
 	}
 
 	return (
@@ -24,6 +33,7 @@ function TodoItem({ label, ...rest }) {
 
 TodoItem.propTypes = {
 	label: PropTypes.string.isRequired,
+	dependencies: PropTypes.object,
 }
 
 export default TodoItem
