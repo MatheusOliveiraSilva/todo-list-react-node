dd6d93f9facc29a38d37589bdabb4cf7e8b3282c
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-08-26 20:28:37 -0300

    some orders changed

--- eslint-config/.eslintrc.js
+++ eslint-config/.eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
-	env: {
-		browser: true,
-		commonjs: true,
-		es2020: true,
-		node: true,
-	},
-	extends: [
-		'plugin:prettier/recommended',
-		'plugin:react/recommended',
-		'prettier/react',
-		'prettier',
-	],
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 11,
-		sourceType: 'module',
-	},
-	plugins: ['react', 'prettier'],
-	rules: {
-		'prettier/prettier': 'error',
-	},
-	settings: {
-		react: {
-			version: 'detect',
-		},
-	},
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'prettier/react',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['prettier', 'react'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 }
