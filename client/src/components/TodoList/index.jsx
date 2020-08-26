89f71a3d4dd40f04aacdf12a6ce30d4e10354587
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-08-26 20:26:08 -0300

    handlers property name changed to dependencies

--- client/src/components/TodoList/index.jsx
+++ client/src/components/TodoList/index.jsx
@@ -3,44 +3,44 @@ import PropTypes from 'prop-types'
 import { FiPlus, FiEdit, FiTrash2 } from 'react-icons/fi'
 
 import {
-	StyledTodoList,
-	TodoListHeader,
-	ButtonAction,
-	ButtonGroup,
-	Content,
+  StyledTodoList,
+  TodoListHeader,
+  ButtonAction,
+  ButtonGroup,
+  Content,
 } from './styles'
 
-function TodoList({ children, name, id, handlers }) {
-	const { handleViewTaskForm, handleDeleteList } = handlers
+function TodoList({ children, name, id, dependencies }) {
+  const { handleViewTaskForm, handleDeleteList } = dependencies
 
-	return (
-		<StyledTodoList>
-			<TodoListHeader>
-				<h2>{name}</h2>
-				<ButtonGroup>
-					<ButtonAction>
-						<FiPlus onClick={() => handleViewTaskForm(id, 'todo')} />
-					</ButtonAction>
+  return (
+    <StyledTodoList>
+      <TodoListHeader>
+        <h2>{name}</h2>
+        <ButtonGroup>
+          <ButtonAction>
+            <FiPlus onClick={() => handleViewTaskForm(id, 'todo')} />
+          </ButtonAction>
 
-					<ButtonAction>
-						<FiEdit onClick={() => handleViewTaskForm(id, 'list')} />
-					</ButtonAction>
+          <ButtonAction>
+            <FiEdit onClick={() => handleViewTaskForm(id, 'list')} />
+          </ButtonAction>
 
-					<ButtonAction>
-						<FiTrash2 onClick={() => handleDeleteList(id)} />
-					</ButtonAction>
-				</ButtonGroup>
-			</TodoListHeader>
-			<Content>{children}</Content>
-		</StyledTodoList>
-	)
+          <ButtonAction>
+            <FiTrash2 onClick={() => handleDeleteList(id)} />
+          </ButtonAction>
+        </ButtonGroup>
+      </TodoListHeader>
+      <Content>{children}</Content>
+    </StyledTodoList>
+  )
 }
 
 TodoList.propTypes = {
-	children: PropTypes.node,
-	name: PropTypes.string.isRequired,
-	id: PropTypes.string,
-	handlers: PropTypes.object,
+  children: PropTypes.node,
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string,
+  dependencies: PropTypes.object,
 }
 
 export default TodoList
