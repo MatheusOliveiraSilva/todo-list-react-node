8651bd9479925b6a92dd4ebdd7aa3aa5099e300a
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-08-27 17:50:55 -0300

    fix the method to use arrays

--- server/src/todo/todo.controller.js
+++ server/src/todo/todo.controller.js
@@ -1,132 +1,132 @@
 const { TodoList } = require('./todo.model')
 
 const getAllLists = async (req, res) => {
-	try {
-		const todoList = await TodoList.find({ user: { _id: req.user._id } })
-			.populate('user')
-			.select('-__v')
+  try {
+    const todoList = await TodoList.find({ user: { _id: req.user._id } })
+      .populate('user')
+      .select('-__v')
 
-		res.send(todoList)
-	} catch (err) {
-		res.send(err)
-	}
+    res.send(todoList)
+  } catch (err) {
+    res.send(err)
+  }
 }
 
 const createList = async (req, res) => {
-	const { name } = req.body
+  const { name } = req.body
 
-	try {
-		const todoList = new TodoList({ name, user: req.user._id })
+  try {
+    const todoList = new TodoList({ name, user: req.user._id })
 
-		await todoList.save()
+    await todoList.save()
 
-		res.send('List created sucessfully')
-	} catch (err) {
-		res.send(err)
-	}
+    res.send('List created sucessfully')
+  } catch (err) {
+    res.send(err)
+  }
 }
 
 const renameList = async (req, res) => {
-	const { name: newName } = req.body
+  const { name: newName } = req.body
 
-	try {
-		const todoList = await TodoList.findById(req.params.id)
+  try {
+    const todoList = await TodoList.findById(req.params.id)
 
-		if (todoList.name === newName)
-			return res
-				.status(404)
-				.send('The new name must be different of the old one.')
+    if (todoList.name === newName)
+      return res
+        .status(404)
+        .send('The new name must be different of the old one.')
 
-		todoList.name = newName
+    todoList.name = newName
 
-		todoList.save()
+    todoList.save()
 
-		res.send('List renamed')
-	} catch (err) {
-		res.send(err)
-	}
+    res.send('List renamed')
+  } catch (err) {
+    res.send(err)
+  }
 }
 
 const deleteList = async (req, res) => {
-	try {
-		await TodoList.findByIdAndRemove(req.params.id)
-		res.send('List deleted successfully.')
-	} catch (err) {
-		res.send(err)
-	}
+  try {
+    await TodoList.findByIdAndRemove(req.params.id)
+    res.send('List deleted successfully.')
+  } catch (err) {
+    res.send(err)
+  }
 }
 
 const addTodo = async (req, res) => {
-	const { task } = req.body
+  const { task } = req.body
 
-	try {
-		const todoList = await TodoList.findById(req.params.id)
+  try {
+    const todoList = await TodoList.findById(req.params.id)
 
-		if (!task || task === '')
-			return res.status(404).send('Insert a valid task.')
+    if (!task || task === '')
+      return res.status(404).send('Insert a valid task.')
 
-		if (todoList.tasks.includes(task))
-			return res.status(404).send('This task already exist in the list.')
+    if (todoList.tasks.includes(task))
+      return res.status(404).send('This task already exist in the list.')
 
-		todoList.tasks = [...todoList.tasks, task]
+    todoList.tasks = [...todoList.tasks, task]
 
-		await todoList.save()
+    await todoList.save()
 
-		res.send('Todo added successfully.')
-	} catch (err) {
-		res.send(err)
-	}
+    res.send('Todo added successfully.')
+  } catch (err) {
+    res.send(err)
+  }
 }
 
 const removeTodo = async (req, res) => {
-	const { task } = req.body
+  const { tasks } = req.body
 
-	try {
-		const todoList = await TodoList.findById(req.params.id)
+  try {
+    const todoList = await TodoList.findById(req.params.id)
 
-		if (!task || task === '')
-			return res.status(404).send('Insert a valid task.')
+    if (!tasks || tasks === [])
+      return res.status(404).send('Insert at least one task')
 
-		todoList.tasks = todoList.tasks.filter(item => item !== task)
+    todoList.tasks = todoList.tasks.filter(task => !tasks.includes(task))
 
-		await todoList.save()
+    await todoList.save()
 
-		res.send('Todo removed successfully.')
-	} catch (err) {
-		res.send(err)
-	}
+    res.send('Todo removed successfully.')
+  } catch (err) {
+    res.send(err)
+  }
 }
 
 const renameTodo = async (req, res) => {
-	const { oldName, newName } = req.body
+  const { oldName, newName } = req.body
 
-	try {
-		const todoList = await TodoList.findById(req.params.id)
+  try {
+    const todoList = await TodoList.findById(req.params.id)
 
-		if (!todoList.tasks.includes(oldName))
-			return res.status(404).send('This task does not exist in the list.')
+    if (!todoList.tasks.includes(oldName))
+      return res.status(404).send('This task does not exist in the list.')
 
-		if (todoList.tasks.includes(newName))
-			return res.status(404).send('The new task already exist in the list.')
+    if (todoList.tasks.includes(newName))
+      return res.status(404).send('The new task already exist in the list.')
 
-		const index = todoList.tasks.indexOf(oldName)
+    const index = todoList.tasks.indexOf(oldName)
 
-		todoList.tasks.splice(index, 1, newName)
+    todoList.tasks.splice(index, 1, newName)
 
-		await todoList.save()
+    await todoList.save()
 
-		res.send('Task renamed successfully.')
-	} catch (err) {
-		res.send(err)
-	}
+    res.send('Task renamed successfully.')
+  } catch (err) {
+    res.send(err)
+  }
 }
 
 module.exports = {
-	getAllLists,
-	createList,
-	renameList,
-	deleteList,
-	addTodo,
-	removeTodo,
-	renameTodo,
+  getAllLists,
+  createList,
+  renameList,
+  deleteList,
+  addTodo,
+  removeTodo,
+  renameTodo,
 }
