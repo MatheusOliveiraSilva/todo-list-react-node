4f73425335f1ffdc070e9bfb19ebbf1e38447355
MatheusOliveiraSilva <matheus.oliveira.s@protonmail.com>
2020-08-27 17:52:06 -0300

    rename and delete todo functionalities added

--- client/src/components/OptionsBar/index.jsx
+++ client/src/components/OptionsBar/index.jsx
@@ -1,34 +1,37 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { FiEdit, FiTrash2, FiXCircle } from 'react-icons/fi'
 import { StyledOptionsBar } from './styles'
 
-function OptionsBar({ view, innerView, setInnerView }) {
-	return (
-		<StyledOptionsBar view={view ? 1 : 0} innerView={innerView}>
-			<div>
-				<button>
-					<FiEdit />
-				</button>
+function OptionsBar({ view, innerView, setInnerView, dependencies }) {
+  const { handleViewTaskForm, handleDeleteTask } = dependencies
 
-				<button>
-					<FiTrash2 />
-				</button>
-			</div>
+  return (
+    <StyledOptionsBar view={view ? 1 : 0} innerView={innerView}>
+      <div>
+        <button>
+          <FiEdit onClick={() => handleViewTaskForm('todo', 'rename')} />
+        </button>
 
-			<div>
-				<button>
-					<FiXCircle onClick={() => setInnerView(false)} />
-				</button>
-			</div>
-		</StyledOptionsBar>
-	)
+        <button>
+          <FiTrash2 onClick={handleDeleteTask} />
+        </button>
+      </div>
+
+      <div>
+        <button>
+          <FiXCircle onClick={() => setInnerView(false)} />
+        </button>
+      </div>
+    </StyledOptionsBar>
+  )
 }
 
 OptionsBar.propTypes = {
-	view: PropTypes.bool.isRequired,
-	innerView: PropTypes.bool.isRequired,
-	setInnerView: PropTypes.func.isRequired,
+  view: PropTypes.bool.isRequired,
+  innerView: PropTypes.bool.isRequired,
+  setInnerView: PropTypes.func.isRequired,
+  dependencies: PropTypes.object,
 }
 
 export default OptionsBar
